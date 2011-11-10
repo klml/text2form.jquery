df05915ac77f4b8bf33184cbe0e383f78dd76e44
klml <github@klml.de>
2011-11-10 01:27:31 +0100

    removed bugs and more jquery

--- js/jquery.text2form.js
+++ js/jquery.text2form.js
@@ -1,37 +1,44 @@
-var icont2f = "<a href='#' class='t2f_icon' onclick='t2f_load ()' ><img src='css/t2f_50.png' alt='text2form' /></a>" ;
-
-var textfieldt2f = "<div id='t2f' title='Text2Form'><textarea name='t2f_textfield' onkeyup='t2f_t2f (value)' ></textarea><br /><a href='#' onclick='t2f_tab2new ()' >tabs auflösen</a></div>"
-
-var DOMstart = '#EditView' ;
-
-
+var textfieldt2f = "<div id='t2f' ><textarea></textarea><br />make2newline: <button>tabs</button><button>blanklines</button><button>spaces</button></div>"
+var DOMstart = 'body' ;
+var forms = 'input:text' ;
 
 jQuery(document).ready(function() {
- $(icont2f).appendTo(document.body);
+ $("<button class='t2f_icon'>t2f</button>").appendTo(document.body);
+ $('.t2f_icon').click(function() {     
+  t2f_load() ;
+ });
 });
 
 function t2f_load () {
- allInputs = $(DOMstart + ' input:text'); // call DomKnoten grap all inputs type=text
- allInputs.each(function(i) {
-  $(this).addClass("farbe" + (i) ); // farben verteilen
-  $(this).attr( { 'onblur' : 't2f_f2t (value)' } )  ; // use 'onblur' while typing or 'onchange' 
- });
-  $("body").append(textfieldt2f); // t2ffieldset einbauen
-  $( "#t2f" ).dialog( { show: 'blind', position: ['right', 150], }); // line is optional http://docs.jquery.com/UI/Dialog
-  t2f_f2t () ; // first call to import existing data
-}
+  allInputs = $(DOMstart + ' ' + forms);
+  allInputs.each(function(i) {
+   $(this).addClass("rainbow" + (i) ); // spread colors
+   $(this).attr( { 'onblur' : 't2f_f2t (value)' } )  ; // use 'onblur' while typing or 'onchange' 
+  });
 
-function t2f_tab2new () {
- var t2f_textfield = $(".t2f textarea") ;
- var t2f_textfieldnew = t2f_textfield.val.replace("klml", "jjjj");
- $("#t2f textarea").append( t2f_textfieldnew.val() );  //using tabs !!doesnt work!!
+   $("body").append(textfieldt2f);
+   $( "#t2f" ).dialog( { show: 'blind', position: ['right', 150], }); // line is optional http://docs.jquery.com/UI/Dialog
+   t2f_f2t () ; // import existing data first
+
+   $('#t2f textarea').keyup(function() {
+    t2f_t2f( $(this).val() );
+   });
+
+   $('#t2f button').click(function() {
+    switch ($(this).html()) {
+      case "tabs":
+        replace = /\t/g ;
+      break;
+      case "blanklines":
+        replace = /\n\n/g ;
+      break;
+      case "spaces":
+        replace = /\s/g ;
+      break;
+      }
+      t2f_2newline ( replace ) ;
+   });
 }
-function t2f_f2t () { // import existing data
- $("#t2f textarea").text( '' ) ; // clear first
-  allInputs.each(function(i) {  
-  $("#t2f textarea").append( $(this).val() + '\n' ) ;
- });
-};
 function t2f_t2f (t2f_textfield) { 
  formelement = new Array();
  formelement = t2f_textfield.split("\n") ; // allocate text
@@ -39,3 +46,14 @@ function t2f_t2f (t2f_textfield) {
   $(this).val(formelement[i]);
  }); 
 };
+function t2f_f2t () { // import existing data
+ $("#t2f textarea").text( '' ) ; // clear first
+  allInputs.each(function(i) {  
+  $("#t2f textarea").append( $(this).val() + '\n' ) ;
+ });
+};
+function t2f_2newline ( char ) {
+ var t2f_textfieldnew = $("#t2f textarea").val().replace(char, '\n');
+ $("#t2f textarea").val( t2f_textfieldnew );
+ t2f_t2f($("#t2f textarea").val());
+}
